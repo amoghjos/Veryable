6793293855f22a0d7cf33209f8342082400dbf64
Amogh Joshi <amogh@craftedthoughts.com>
2023-02-22 13:07:52 -0800

    AccountListTableViewCell

---
+++ Veryable Sample/View/AccountListTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  AccountListTableViewCell.swift
+//  Veryable Sample
+//
+//  Created by Amogh Joshi on 2/22/23.
+//  Copyright © 2023 Veryable Inc. All rights reserved.
+//
+
+import UIKit
+
+class AccountListTableViewCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
